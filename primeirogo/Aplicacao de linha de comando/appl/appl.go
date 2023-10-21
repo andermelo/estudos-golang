a288674003c64e6e01e13bdf58120ac01516a289
andermelo <anderson.90@gmail.com>
2023-10-20 23:35:07 -0300

    updates estudos

--- primeirogo/Aplicacao de linha de comando/appl/appl.go
+++ primeirogo/Aplicacao de linha de comando/appl/appl.go
@@ -1,6 +1,12 @@
 package appl
 
-import "github.com/urfave/cli"
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/urfave/cli"
+)
 
 // Gerar is show
 func Gerar() *cli.App {
@@ -8,5 +14,53 @@ func Gerar() *cli.App {
 	app.Name = "App de linha de comando"
 	app.Usage = "Buscar ips e nomes na internet"
 
+	flags := []cli.Flag{
+		cli.StringFlag{
+			Name:  "host",
+			Value: "anderson.mobi",
+		},
+	}
+
+	app.Commands = []cli.Command{
+		{
+			Name:   "ip",
+			Usage:  "Busca IPs de enderecos na internet",
+			Flags:  flags,
+			Action: buscarIps,
+		},
+		{
+			Name:   "servidores",
+			Usage:  "Busca o nome do servidores na internet",
+			Flags:  flags,
+			Action: buscarservidores,
+		},
+	}
+
 	return app
+}
+
+func buscarIps(c *cli.Context) {
+	host := c.String("host")
+
+	ips, erro := net.LookupIP(host)
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	for _, ip := range ips {
+		fmt.Println(ip)
+	}
+}
+
+func buscarservidores(c *cli.Context) {
+	host := c.String("host")
+
+	servidores, erro := net.LookupNS(host)
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	for _, servidor := range servidores {
+		fmt.Println(servidor.Host)
+	}
 }
